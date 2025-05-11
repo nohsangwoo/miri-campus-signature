833ae4395b025fe118e0bd60cf69ac21f10dfe6b
nohsangwoo <nsgr12@naver.com>
2025-05-11 15:20:15 +0900

    feat: 미리 캠퍼스 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'miri campus',
+  description: 'miri campus',
+  generator: 'miri campus',
 }
 
 export default function RootLayout({
